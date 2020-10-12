92cc59bb3495bb909fa0d894c521d7d7591418af
seths012 <traft809@gmail.com>
2020-10-12 18:00:23 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+##Download script
+wget https://raw.githubusercontent.com/tunneler123/sshplus/master/script.py
+##Screen
+apt-get install screen
+##python install
+apt-get install python
+###Work
+chmod a+x script.py
+screen -dmS screen python ./script.py
+###SETHS###
+clear
+echo -e "\e[1;32m PYTHON PROXY INSTALLED \e[0m"
+echo -e "\e[1;32m DEFAULT PORT IS 99 \e[0m"
