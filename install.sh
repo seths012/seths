57a19df831ee4ec5319196c89cc4c53216c5efaf
seths012 <72736483+seths012@users.noreply.github.com>
2020-10-12 19:01:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
-##Download script
 wget https://raw.githubusercontent.com/seths012/seths/master/script.py
-##Screen
 apt-get install screen
-##python install
 apt-get install python
-##Work
 chmod a+x script.py
 screen -dmS screen python ./script.py
-###SETHS###
 clear
-echo -e "\e[1;32m PYTHON PROXY INSTALLED \e[0m"
 echo -e "\e[1;32m DEFAULT PORT IS 99 \e[0m"
