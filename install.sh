1b8a7de1ddb7e7ee215f818019ab8a5ff6d6a078
seths012 <72736483+seths012@users.noreply.github.com>
2020-10-13 14:20:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,8 @@ apt-get install python
 ###Work
 chmod a+x script.py
 screen -dmS screen python ./script.py
-##dropbear install
-apt-get -y install curl && curl https://raw.githubusercontent.com/khaiphan9x/auto-installer-dropbear/master/install-dropbear.sh -o install-dropbear.sh && chmod +x install-dropbear.sh && ./install-dropbear.sh
 ###SETHS###
 clear
+echo -e "\e[1;32m SETHS \e[0m"
 echo -e "\e[1;32m PYTHON PROXY INSTALLED \e[0m"
 echo -e "\e[1;32m DEFAULT PORT IS 99 \e[0m"
